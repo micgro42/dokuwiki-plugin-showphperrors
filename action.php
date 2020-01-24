0c3baafa0d13fc6d53a44682b731f859c8da7a94
Michael Große <mic.grosse@googlemail.com>
2020-01-24 23:57:13 +0100

    Minor style fixes
    That guard is not need and files that declare stuff shouldn't contain
    executable code.

--- action.php
+++ action.php
@@ -6,9 +6,6 @@
  * @author  Michael <mic.grosse@posteo.de>
  */
 
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
-
 class action_plugin_showphperrors extends DokuWiki_Action_Plugin {
 
     /**
@@ -18,9 +15,7 @@ class action_plugin_showphperrors extends DokuWiki_Action_Plugin {
      * @return void
      */
     public function register(Doku_Event_Handler $controller) {
-
        $controller->register_hook('INIT_LANG_LOAD', 'AFTER', $this, 'handle_dokuwiki_started');
-
     }
 
     /**
@@ -47,5 +42,3 @@ class action_plugin_showphperrors extends DokuWiki_Action_Plugin {
     }
 
 }
-
-// vim:ts=4:sw=4:et:
